44eb55ddcef8a8c08727a8a0d887d5226be25237
skyemm <yangmin_mm@163.com>
2019-07-25 23:38:17 +0800

    动画

--- src/components/shopping/shoppingInfo.vue
+++ src/components/shopping/shoppingInfo.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="shoppingInfo">
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="flag" ref="ball"></div>
+    </transition>
     <!-- 轮播图 -->
     <!-- <div class="sw-content">
       <div class="sw-body">
@@ -27,10 +30,10 @@
         </p>
         <p>
           购买数量:
-          <numBox></numBox>
+          <numBox v-on:componentNumber='getNumber' :Max='goodsInfo.stock_quantity'></numBox>
         </p>
         <mt-button type="primary" size="small">&nbsp;&nbsp;&nbsp;立即购买&nbsp;&nbsp;&nbsp;</mt-button>
-        <mt-button type="danger" size="small">加入购物车</mt-button>
+        <mt-button type="danger" size="small" @click="flag=!flag">加入购物车</mt-button>
       </div>
     </div>
 
@@ -61,12 +64,17 @@ export default {
     return {
       lunbotu: [], //轮播图数组
       id: this.$route.params.id, //获取路由中的图片id
-      goodsInfo:{}  //存储商品信息
+      goodsInfo: {}, //存储商品信息
+      flag: false, //确定小球是否显示
+      number:1,//存放商品数量
     };
   },
   created() {
     //data和methods已经准备好
     this.getLunbotu(); //获取轮播图数据
+    // this.getGoodsInfo();
+  },
+  mounted() {
     this.getGoodsInfo();
   },
   methods: {
@@ -84,18 +92,67 @@ export default {
     },
 
     //获取商品信息
-    getGoodsInfo(){
-      this.$http.get("api/goods/getinfo/" + this.id).then(result=>{
-        if(result.body.status===0){
-          this.goodsInfo=result.body.message[0];
+    getGoodsInfo() {
+      this.$http.get("api/goods/getinfo/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.goodsInfo = result.body.message[0];
         }
-      })
+      });
     },
-    goDesc(id){
-      this.$router.push({path:'/home/shoppingDesc/'+id})
+    goDesc(id) {
+      this.$router.push({ path: "/home/shoppingDesc/" + id });
     },
-    goComment(id){
-      this.$router.push({path:'/home/shoppingComment/'+id})
+    goComment(id) {
+      this.$router.push({ path: "/home/shoppingComment/" + id });
+    },
+//小球动画效果
+    beforeEnter: function(el) {
+      el.style.transform = "translate(0,0)";
+    },
+    enter: function(el, done) {
+      el.offsetWidth;
+
+      //小球不准确的原因
+      //不能写死最终位置，因为滚动条会发生改变，根据获取到的坐标动态设置小球的最终位置
+      //得出解题思路： 先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，然后 让 y 值 求差， x 值也求 差，得到 的结果，就是横纵坐标要位移的距离
+      //  如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+
+
+      // 获取小球的 在页面中的位置
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
+      // 获取 徽标 在页面中的位置
+      const badgePosition = document
+        .getElementById("badge")
+        .getBoundingClientRect();
+
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
+
+      el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+      el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+     
+      done();//应用afterEnter（）
+    },
+    afterEnter: function(el) {
+      //这句话的第一个功能是，控制小球的显示和隐藏
+      //第二个功能是,直接跳过后半场动画，让flag标识符直接变为false
+      //第二次点击按钮时，flag false->true
+      this.flag = !this.flag;
+
+      //vue把一个完整的动画拆分为了两部分
+      //我们使用flag来表示动画的切换
+      //刚开始，flag=false->  true  ->false
+    },
+
+    getNumber(num){
+      if(num<=0){
+        this.number=1;
+      }else if(num>this.goodsInfo.stock_quantity){
+        this.number=this.goodsInfo.stock_quantity;
+      }else{
+        this.number=num;
+      }
+      console.log(this.number);
     }
   },
   components: {
@@ -142,6 +199,7 @@ export default {
         .old {
           font-size: 14px;
           padding-right: 10px;
+          text-decoration: line-through;
         }
       }
     }
@@ -166,12 +224,22 @@ export default {
       .setbutton {
         margin: 0 10px 20px 10px;
         width: 94%;
-        
+
         .mint-button {
           margin-top: 20px;
         }
       }
     }
+  }
+  .ball {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: #c7254e;
+    position: absolute;
+    z-index: 99;
+    top: 363px;
+    left: 127px;
   }
 }
 </style>
